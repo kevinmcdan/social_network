16035683a59bf21c8d24e2038c2d8dc54404e24e
Kevin <k3vinmcd@gmail.com>
2023-10-28 05:37:53 +0000

    Finish registration, login, and messaging system

--- social_project/dwitter/views.py
+++ social_project/dwitter/views.py
@@ -2,28 +2,29 @@ from django.shortcuts import render, redirect
 from .forms import DweetForm
 from .models import Dweet, Profile
 from operator import attrgetter
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def dashboard(request):
-    if request.user.is_authenticated:
-        form = DweetForm(request.POST or None)
-        if request.method == "POST":
-            if form.is_valid():
-                dweet = form.save(commit=False)
-                dweet.user = request.user
-                dweet.save()
-                return redirect("dwitter:dashboard")
-        followed_dweets = Dweet.objects.filter(user__profile__in=request.user.profile.follows.all()).order_by("-created_at")
-        # print(followed_dweets.query) # uncomment when debugging
-        return render(request, "dwitter/dashboard.html", {"form": form, "dweets": followed_dweets},)
-    else:
-        return redirect("users:login")
+    form = DweetForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            dweet = form.save(commit=False)
+            dweet.user = request.user
+            dweet.save()
+            return redirect("dwitter:dashboard")
+    followed_dweets = Dweet.objects.filter(user__profile__in=request.user.profile.follows.all()).order_by("-created_at")
+    # print(followed_dweets.query) # uncomment when debugging
+    return render(request, "dwitter/dashboard.html", {"form": form, "dweets": followed_dweets},)
 
+@login_required
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
 
+@login_required
 def profile(request, pk):
     if not hasattr(request.user, 'profile'):
         missing_profile = Profile(user=request.user)
--- social_project/users/views.py
+++ social_project/users/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -23,3 +25,39 @@ def register(request):
             user.save()
             login(request, user)
             return redirect(reverse("dwitter:dashboard"))
+
+def see_request(request):
+    text = f"""
+        Some attributes of the HttpRequest object:
+
+        scheme: {request.scheme}
+        path:   {request.path}
+        method: {request.method}
+        GET:    {request.GET}
+        user:   {request.user}
+    """
+
+    return HttpResponse(text, content_type="text/plain")
+
+def user_info(request):
+    text = f"""
+        Selected HttpRequest.user attributes:
+
+        username:     {request.user.username}
+        is_anonymous: {request.user.is_anonymous}
+        is_staff:     {request.user.is_staff}
+        is_superuser: {request.user.is_superuser}
+        is_active:    {request.user.is_active}
+    """
+
+    return HttpResponse(text, content_type="text/plain")
+
+from django.contrib import messages
+
+@login_required
+def add_messages(request):
+    username = request.user.username
+    messages.add_message(request, messages.INFO, f"Hello {username}")
+    messages.add_message(request, messages.WARNING, "DANGER WILL ROBINSON")
+
+    return HttpResponse("Messages added", content_type="text/plain")
