12ab1f0ce67f7febf554c4142fcdf6f373739b8a
kevinmcdan <k3vinmcd@gmail.com>
2023-11-04 07:46:44 +0000

    finished initial version of makefile

--- social_project/social_project/settings.py
+++ social_project/social_project/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -140,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "dwitter:dashboard"
 
-LOGOUT_REDIRECT_URL = "users:login"
+LOGOUT_REDIRECT_URL = "login"
 
 EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = 587
